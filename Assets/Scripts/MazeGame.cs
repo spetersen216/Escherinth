94fcfe675ac6b02332ab071e46b1ef673070160f
=hakr1337 <=>
2015-02-24 01:46:35 -0700

    key, skybox,lighting, sizing

--- Assets/Scripts/MazeGame.cs
+++ Assets/Scripts/MazeGame.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MazeGame : MonoBehaviour {
 
-	private MazeTool maze;
+	public GameObject maze;
+	private MazeTool mazeTool;
+	public IEnumerable walls;
 	public Key key;
 	private Point3 key_loc;
 	private GameObject door;
@@ -11,7 +14,10 @@ public class MazeGame : MonoBehaviour {
 	private GameObject plane;
 	public OVRPlayerController player_control;
 	private MazeStructure mazeStruct;
-	private Transform walls;
+	//private Transform walls;
+	private GameObject left_cam;
+	private GameObject right_cam;
+
 	//private OVRMainMenu menu;
 	//private bool inMenu = false;
 
@@ -35,18 +41,50 @@ public class MazeGame : MonoBehaviour {
 
 
 	private void createObjects(){
-		map = new GameObject ();
-		plane = GameObject.CreatePrimitive (PrimitiveType.Plane);
+		//map = new GameObject ("Map");
+		//plane = GameObject.CreatePrimitive (PrimitiveType.Plane);
+		//plane.transform.SetParent (map.transform);
 		//plane.transform.position = new Vector3 (0, 1, 0);
-		plane.transform.localScale = new Vector3 (30, 30, 30);
-		plane.transform.position = new Vector3(50,1,50);
-		Instantiate (player_control, new Vector3 (0, 3, 0), Quaternion.identity);
-		//Instantiate (maze, new Vector3 (0, 0, 0), Quaternion.identity);
-		maze = map.AddComponent<MazeTool>();
-		//maze.transform.localScale = new Vector3(10,10,10);
-		walls = maze.wallContainer;
-		map.transform.localPosition = new Vector3 (0f, 33.2f, 0f);
-		map.transform.localScale = new Vector3 (500, 500, 500);
-		
+
+		Instantiate (key, new Vector3 (0, 0, 0), Quaternion.identity);
+		//key.tag = "collectable";
+
+		//map = Instantiate (maze, new Vector3 (488.9f, -304f, -274.2f), Quaternion.identity) as GameObject;
+		maze = GameObject.Find ("Maze");
+		//mazeTool = GameObject.Find (""+map.name).GetComponent<MazeTool>();
+		mazeTool = (MazeTool)FindObjectOfType(typeof(MazeTool));
+		//mazeStruct = new MazeStructure (mazeTool);
+//		Debug.Log ("" + map.name);	
+
+		//plane.transform.position = new Vector3(50,1,50);
+		Instantiate (player_control, new Vector3 (1, 1, 1), Quaternion.identity);
+		left_cam = GameObject.Find ("LeftEyeAnchor");
+		right_cam = GameObject.Find("RightEyeAnchor");
+
+		left_cam.gameObject.AddComponent<Skybox> ().material = 
+			(Material)Resources.Load ("Overcast2 Skybox", typeof(Material));
+		right_cam.gameObject.AddComponent<Skybox> ().material = 
+			(Material)Resources.Load ("Overcast2 Skybox", typeof(Material));
+		/*walls = mazeTool.walls;
+		//GameObject[] ws = Object.FindObjectsOfType (typeof(MazeToolWall)) as GameObject[];
+		string[] names = new string[100];
+		for (int i = 0; i < mazeTool.width+9; i++) {
+			for (int j = 0; j < mazeTool.width+9; j++) {
+				string s = "wall " + i + " " + j;
+				names[j] = s;
+			}
+		}
+		GameObject[] w = new GameObject[100];
+		int count = 0;
+		foreach (string s in names) {
+			w[count] = GameObject.Find (s);
+			count++;
+		}*/
+		//Debug.Log(""+ mazeStruct.FindKey().ToString());
+		//key.transform.position.Set (mazeStruct.FindKey().x, mazeStruct.FindKey().y, mazeStruct.FindKey().z);
+		//player_control.gameObject.AddComponent<CollectionMechanic> ();
+
+		//key.tag = "collectable";
+
 	}
 }
