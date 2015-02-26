49634295e7a70c5ef9e8a4a1bfc7f66d17617ddc
= <=>
2015-02-26 14:52:08 -0700

    working playthrough

---
+++ Assets/OVR/Scripts/Util/RunTime.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunTime : MonoBehaviour {
+
+	private LightSystem lights;
+	private GameObject door;
+	private Key key;
+	public Material skyboxMaterial;
+
+
+	public void Init(LightSystem lights, GameObject door, Key key, Material mat)
+	{
+		this.lights = lights;
+		this.door = door;
+		this.key = key;
+		skyboxMaterial = mat;
+	}
+
+
+	// The OnTriggerEnter function is called when the collider attached to this game object (whatever object the script is attached to) overlaps another collider set to be a "trigger"
+	void OnTriggerEnter(Collider collider)
+	{
+		// We want to check if the thing we're colliding with is a collectable, this will differentiate it from other trigger objects which we might add in the future
+		if (collider.GetComponent<Key>() == key)
+		{
+			//Debug.Log("Collision with collection");
+			door.SetActive(false);
+			collider.gameObject.SetActive(false);
+			lights.keyTime = 0;
+			print (skyboxMaterial.GetColor("_Tint"));
+			skyboxMaterial.SetColor("_Tint", new Color32((byte)44, (byte)28, (byte)53, (byte)128));
+			gameObject.GetComponent<OVRPlayerController>().Acceleration = 0.3f;
+		}
+	}
+}
